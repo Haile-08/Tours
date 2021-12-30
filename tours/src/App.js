73bb29c12478725dbbecb3a08d2ca373fbc7755e
Haile Melaku <haile4cmd@gmail.com>
2021-12-30 14:04:12 +0300

    finished

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,33 +1,57 @@
-import react,{useState,useEffect} from 'react';
-
-import Loading from './components/Loading/Loading';
-import Suggestion from './components/Suggestion/Suggestion';
-import './App.css';
-
-const url = 'https://www.boredapi.com/api/activity'
+import React, { useState, useEffect } from "react";
+import Loading from "./components/Loading/Loading";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [Loading,setLoading] = useState(true);
-  const [Suggestion,setSuggestion] = useState([]);
+  //the useState hooks
+  const [idea, setIdea] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [count, setcount] = useState(0);
 
-  const fetchidea = async ()=>{
-    setLoading(true);
-    try{
-      const res = await fetch(url);
-      const Suggestion = await res.json();
+  //Data fetching function
+  async function fetchdata() {
+    try {
+      const res = await axios.get("http://www.boredapi.com/api/activity/");
+      setIdea(res.data);
       setLoading(false);
-      setSuggestion(Suggestion);
-    }catch(error){
-      setLoading(false);
+    } catch (error) {
       console.log(error);
+      alert("data not fetched" + `   ${error}`);
     }
-  };
+  }
+
+  // useEffect hook
+  useEffect(() => {
+    fetchdata();
+  }, [count]);
+
+  // btn management
+  function clicked() {
+    setcount(count + 1);
+  }
+  //main return
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-          
-      </header>   
-    </div>
+    <>
+      <main className="main w-screen h-screen bg-slate-700 flex flex-col justify-center items-center">
+        <div className="cont container w-50 py-auto px-48 bg-lime-400 flex flex-col justify-center items-center rounded-lg">
+          <h1 className="mag text-black text-9xl font-mono">bored api</h1>
+          <div>
+            <h1>Idea = {idea.activity}</h1>
+            <a href={idea.link}></a>
+          </div>
+          <button
+            className="btn px-12 py-9 bg-blue-800 my-9 rounded-lg"
+            onClick={clicked}
+          >
+            Change
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
 
--- tours/src/components/Loading/Loading.js
+++ tours/src/components/Loading/Loading.js
@@ -1,12 +1,25 @@
-import React from "react";
-import './Loading.css';
+import React, { useState } from "react";
+import { css } from "@emotion/react";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import "./Loading.css";
 
-function Loading(){
-    return(
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
+function Loading() {
+  let [color, setColor] = useState("#36D745");
+  return (
     <React.Fragment>
-
+      <main className="main bg-slate-700 w-screen h-screen flex flex-col justify-center items-center">
+        <h1 className=" text-2xl  text-white ">
+          <PacmanLoader color={color} css={override} size={60} />
+        </h1>
+      </main>
     </React.Fragment>
-    );
+  );
 }
 
 export default Loading;
